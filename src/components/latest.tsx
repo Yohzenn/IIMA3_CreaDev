3888ba42659aac04c80ae614b5de50bea88d7dd3
Yohan Seneret <yohan.seneret@gmail.com>
2026-01-14 16:53:38 +0100

    added latest component

--- src/components/latest.tsx
+++ src/components/latest.tsx
@@ -4,13 +4,17 @@ import jump_disk from "../assets/img/jump_disk.png"
 
 function Latest(){
     return (
-        <>
-            <div className="bg-gradient-to-b from-[#6A3F4A] to-[#170C10] py-20">
+            <div className="bg-gradient-to-b from-[#6A3F4A] to-[#170C10] py-20 w-screen">
                 <h1 className="text-center font-[primetime] m-20 text-6xl text-white"> Latest Release </h1>
-                <div className="w-[80%] m-auto flex justify-between">
-                    <img src={jump_cover} />
-                    <img src={jump_disk} className="w-xl absolute" />
-                    <div className="flex flex-col justify-between">
+                <div className="w-[80%] m-auto flex justify-between gap-20">
+
+                        <div className="relative w-[382]">
+                            <img src={jump_cover} className="relative z-20" />
+                            <img src={jump_disk} className="absolute top-[0] bottom-[0] left-[50%] w-[500px] z-10 "/>
+                        </div>
+                        
+
+                    <div className="flex flex-col justify-between h-[382px]">
                         <div>
                             <p className="font-[primetime] text-[#F79AAF] text-center text-6xl mb-4">Jump</p>
                             <div className="w-full border-2 border-[#F79AAF]"></div>
@@ -21,7 +25,6 @@ function Latest(){
                 </div>
                 
             </div>
-        </>
     )
 }
 
