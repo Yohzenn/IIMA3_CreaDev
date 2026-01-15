2191c5a93a3e376eae736b03a96451fa6e34fe03
Emilie Xu <emilie.xu@outlook.com>
2026-01-15 09:14:06 +0100

    Add banner component

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Latest from "./components/latest";
 import MembersSection from "./components/membersSection";
+import Banner from "./components/banner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Banner />
     <Latest></Latest>
     <MembersSection />
   </StrictMode>
