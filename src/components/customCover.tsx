9a3b7284c522b49042054b1b68abc16089f6a613
Yohan Seneret <yohan.seneret@gmail.com>
2026-01-16 13:18:42 +0100

    added Template CustomCover

---
+++ src/components/customCover.tsx
@@ -0,0 +1,41 @@
+function CustomCover () {
+    return (
+        <div className="">
+            <h1 className="text-center font-[primetime] text-6xl m-20">Custom Your Cover</h1>
+            <div className="flex justify-around ">
+                <div className="w-[360px] h-[382px] bg-white"></div>
+                <div className="w-4 h-[382px] bg-white"></div>
+                <div className="w-[360px] font-[primetime] text-center">
+                    <div>
+                        <h3 className="mb-5">Logo color:</h3>
+                            <div className="flex gap-8 justify-center mb-5">
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                        </div>
+
+                        <h3 className="mb-5">Cover:</h3>
+                            <div className="flex gap-8 justify-center mb-5">
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                        </div>
+
+                        <h3 className="mb-5">Background:</h3>
+                            <div className="flex gap-8 justify-center mb-5">
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                            <div className="bg-white w-14 h-14 "></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default CustomCover
